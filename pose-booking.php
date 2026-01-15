c952455e87ddfe44c871b17a2061dc0adcedf339
Elegent Admin <admin@elegentkw.local>
2026-01-15 06:25:00 +0300

    Fix: Force price to be white in dark mode

--- pose-booking.php
+++ pose-booking.php
@@ -16,7 +16,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Plugin Constants
-define('POSE_BOOKING_VERSION', '2.0.7');
+define('POSE_BOOKING_VERSION', '2.0.8');
 define('POSE_BOOKING_PATH', plugin_dir_path(__FILE__));
 define('POSE_BOOKING_URL', plugin_dir_url(__FILE__));
 
